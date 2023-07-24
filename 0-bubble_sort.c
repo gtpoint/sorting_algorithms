4d0b9504cf52e0eef988856a8ca671104335bdbb
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-07-24 16:36:59 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,46 @@
 #include "sort.h"
 
 /**
+ * _swap - Swap two integers in an array.
+ * @a: The first integer.
+ * @b: The second integer.
+ */
+void _swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
  * bubble_sort - Sort an array of integers in ascending order.
  * @array: An array of integers to sort.
  * @size: The size of the array.
  *
  * Description: Prints the array after each swap.
  */
-void bubble_sort(int *array, size_t size) {
-    size_t i, len = size;
-    int tmp;
-    bool cond = false;
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool cond = false;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    while (cond == false) {
-        cond = true;
-        for (i = 0; i < len - 1; i++) {
-            if (array[i] > array[i + 1]) {
-                tmp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = tmp;
-                print_array(array, size);
-                cond = false; // Set to false if any swap occurs
-            }
-        }
-        len--; // Reduce the size of the unsorted portion
-    }
+	while (cond == false)
+	{
+		cond = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				_swap(array + i, array + i + 1);
+				print_array(array, size);
+				cond = false;
+			}
+		}
+		len--;
+	}
 }
