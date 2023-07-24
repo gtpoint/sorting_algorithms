ce821a22d66928d2aed2a8f1fced4a7f8cf1193b
Oussama El aouzari <125877426+gtpoint@users.noreply.github.com>
2023-07-24 16:22:23 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,17 +8,22 @@
  * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size){
-  size_t i;
+  size_t i, len = size;
   int tmp;
+  bool cond = false;
+
   if (array == NULL || size < 2)
 		return;
-  
-  for (i = 0; i < size - 1; i++){
-    if (array[i] > array[i + 1]){
-      tmp = array[i];
-      array[i] = array[i + 1];
-      array[i + 1] = tmp;
-      print_array(array, size);
-    }
+  while (cond == false){
+	  cond = true;
+	  for (i = 0; i < len - 1; i++){
+		  if (array[i] > array[i + 1]){
+			  tmp = array[i];
+			  array[i] = array[i + 1];
+			  array[i + 1] = tmp;
+			  print_array(array, size);
+		  }
+	  }
+	  len--;
   }
 }
